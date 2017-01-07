ab3c281962ae11555f56b73ee42146c4e334409d
aulbert <aalbertuntung@gmail.com>
2017-01-07 15:24:57 -0500

    Slow day

--- 015.py
+++ 015.py
@@ -1,5 +1,3 @@
-
-    
 def twoSum(nums, k):
     lookup = [] 
     # Return list
@@ -11,6 +9,35 @@ def twoSum(nums, k):
             lookup.append(nums[i])
     return retnums
 
+
+#http://stackoverflow.com/questions/480214/how-do-you-remove-duplicates-from-a-list-in-whilst-preserving-order
+def f7(seq):
+    seen = set()
+    seen_add = seen.add 
+    return [x for x in seq if not (x in seen or seen_add(x))]
+
+
+def threeSum(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    length = len(nums)
+    retnums = []
+    counter = 1
+    for i in range(length):
+        result = twoSum(nums[i + 1:], nums[i] * -1)
+        if result != []:
+            for r in result:
+                r.append(nums[i])
+                retnums.append(sorted(r))
+    seen = []
+    for r in retnums:
+        if r not in seen:
+            seen.append(r)
+    return seen
+
+
 if __name__ == '__main__':
-    print twoSum([1,2,3,0], 3)
-    
+    print twoSum([1,2,3,0], 2)
+    r =  threeSum([1,2,3,-1,-2,1,0])
---
+++ 016.py
@@ -0,0 +1,7 @@
+# One solution
+def threeSumClosest(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
