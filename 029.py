105a64d3491c562a52c2a29b0eb43e97ccf1c4e1
aulbert <aalbertuntung@gmail.com>
2017-01-12 23:41:18 -0500

    Grace day J13

--- 029.py
+++ 029.py
@@ -1,16 +1,32 @@
 def divide(dividend, divisor):
 	# Negative integers
 	if divisor == 0:
-		return MAX_INT
+		return 2147483647
 
 	divisorp = abs(divisor)
 	dividendp = abs(dividend)
 
+	# Cases for negative and positive
+	neg = (dividend < 0 and divisor > 0) or (divisor < 0 and dividend > 0)
 	r = 0
 	while dividendp - divisorp >= 0:
 		dividendp = dividendp - divisorp
 		r = r + 1
+	return r if not neg else -r
+
+	# Multiply multiply multiply by 2
+	# If woopsie, go back one
 
 
+# Nice trick from discussion bit division
+    # c,sub=1,divisor;
 
-	return r 
+    # while(dividend >= divisor):
+    #     if(dividend>=sub):
+    #         dividend-=sub;
+    #         ret+=c;
+    #         sub=(sub<<1);
+    #         c=(c<<1);
+    #     else:
+    #         sub=(sub>>1);
+    #         c=(c>>1);
