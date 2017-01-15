cd85e3d519e03b20e3173dd2c7be3373e9a41e98
aulbert <aalbertuntung@gmail.com>
2017-01-14 22:35:21 -0500

    Studying
    —

---
+++ 019.py
@@ -0,0 +1,28 @@
+# Not my solution
+# https://leetcode.com/articles/remove-nth-node-end-list/
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        first = slow = head
+        # Look ahead
+        for i in range(n):
+            fast = fast.next
+        # If n = first element in the list
+        if not fast:
+            return head.next
+        # If there is something after
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head # Return head because slow modifies head
