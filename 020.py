5a246b04be857680165c5d479e5a728b5945d2a9
aulbert <aalbertuntung@gmail.com>
2017-01-14 23:09:40 -0500

    ??
    Works but not accepted

---
+++ 020.py
@@ -0,0 +1,17 @@
+def isValid(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    if len(s) % 2 != 0 or len(s) == 0:
+    	return False
+    lookup = {'(': ')', '[': ']', '{': '}'}
+    for _ in range(0, len(s), 2):
+    	if s[_] in '({[' and s[_ + 1] != lookup[s[_]] or s[_] not in '({[':
+    	   return False
+   	return True
+
+print isValid('())')
+print isValid('([])')
+print isValid('[')
+print isValid('()()()()()(){}[]{}')
