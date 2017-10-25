c8aa278522e1b3e327ba8bb06b7bdb37390bb192
aulbert <aalbertuntung@gmail.com>
2017-10-24 22:11:31 -0400

    Mk

---
+++ 125 - Valid Palindrome.py
@@ -0,0 +1,30 @@
+# ALPHANUMERIC
+import re
+
+alphanumeric = re.compile(r'[a-z0-9]')
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # if not s: return True # covered 
+        s = s.lower()
+        lo, hi = 0, len(s) - 1
+        
+        print(s)
+        while lo <= hi:
+            if not alphanumeric.match(s[lo]):
+                lo += 1
+            elif not alphanumeric.match(s[hi]):
+                hi -= 1
+            else:                
+                if s[lo] != s[hi]: return False
+
+                # Don't forget to advance the pointers
+                lo += 1
+                hi -= 1
+            
+        
+        return True
