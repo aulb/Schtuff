504fb38cc1729637dd9688ffae3ae99096a0114b
aulbert <aalbertuntung@gmail.com>
2017-10-26 19:42:27 -0400

    In order DFS
    For KTH SMALLEST YO

--- 230 - Kth Smallest BST.py
+++ 230 - Kth Smallest BST.py
@@ -5,31 +5,25 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution(object):
 	# Counting the whole damn thing
 	# The counting method has constant space TODO
-	def count(self, root):
-		# if at the end
-		if not root: return 0
-		return self.count(root.left) + self.count(root.right) + right_count + 1
+	def kthSmallest(self, root, k):
+		"""	
+	    :type root: TreeNode
+	    :type k: int
+	    :rtype: int
+	    """
+		# In order traversal would work...
+		count = []
+		self.helper(root, count)
 
-    def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-        # In order traversal would work...
-        count = []
-        self.helper(node, count)
-
-        # Check valid k
-	    if k - 1 < len(count):
-	    	return count[k - 1]
+		# Check valid k
+		if k - 1 < len(count):
+			return count[k - 1]
 		# else... error handle	    	
 
-	def inOrderDFS(self, node, count):
+	def helper(self, node, count): # inOrderDFS
 		# Memory is n
 		if not node: return
 
